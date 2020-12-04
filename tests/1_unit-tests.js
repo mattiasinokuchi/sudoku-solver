da2f27e05aab61247aea76a619e0a54d5ff967db
Mattias Inokuchi <8-b@tuta.io>
2020-12-04 14:52:31 +0000

    added test 'valid row'

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -25,4 +25,12 @@ suite('UnitTests', () => {
       done();
     });
   });
+
+  suite('Row placement', () => {
+    test('Valid row', function(done) {
+      let string = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+      assert.deepEqual(solver.validate(string), '');
+      done();
+    });
+  });
 });
