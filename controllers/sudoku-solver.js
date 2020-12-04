66c44c995a3b0eacd5c08507999e29695899a337
Mattias Inokuchi <8-b@tuta.io>
2020-12-04 14:46:20 +0000

    completed test 'not 81 characters'

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -3,7 +3,9 @@ class SudokuSolver {
   validate(puzzleString) {
     let invalidCharacter = /[^.0-9]/.test(puzzleString);
     if (invalidCharacter) {
-      return { "error": "Invalid characters in puzzle" };
+      return { error: "Invalid characters in puzzle" };
+    } else if (puzzleString.length != 81) {
+      return { error: "Expected puzzle to be 81 characters long" };
     } else {
       return '';
     }
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -15,7 +15,13 @@ suite('UnitTests', () => {
 
     test('Invalid characters', function(done) {
       let string = '..A..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      assert.deepEqual(solver.validate(string), { "error": "Invalid characters in puzzle" });
+      assert.deepEqual(solver.validate(string), { error: "Invalid characters in puzzle" });
+      done();
+    });
+
+    test('Not 81 characters', function(done) {
+      let string = '9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+      assert.deepEqual(solver.validate(string), { error: "Expected puzzle to be 81 characters long" });
       done();
     });
   });
