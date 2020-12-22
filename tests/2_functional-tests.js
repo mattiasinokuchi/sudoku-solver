f610825cf2a79ca7720f87dd5b0daa52c13e106f
Mattias Inokuchi <8-b@tuta.io>
2020-12-22 13:06:02 +0000

    completed POST test 'incorrect length'

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -54,6 +54,21 @@ suite('Functional Tests', () => {
       });
     });
 
+    test('Incorrect length', function(done) {
+      let string = '9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+      chai.request(server)
+      .post('/api/solve')
+      .send({
+        puzzle: string
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.isObject(res.body, true);
+        assert.deepEqual(res.body, { error: 'Expected puzzle to be 81 characters long' });
+        done();
+      });
+    });
+
   });
 });
 
